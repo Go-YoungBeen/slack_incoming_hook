55f1c2da71ebced3057f87490041e8de67cc9dc7
YoungBeen <120788477+Go-YoungBeen@users.noreply.github.com>
2025-02-20 18:17:09 +0900

    ㅁㅇㅇ

--- Bot.java
+++ Bot.java
@@ -1,28 +1,30 @@
 import java.net.*;
 import java.net.http.*;
-import java.time.*;
-import java.util.*;
 
 public class Bot {
-    public static void main(String[] args) { // 진입부분
-        // 이게 있어야 이 클래스를 실행했을 때 작동을 함
-        // 웹훅을 만들 거임 -> URL 필요함
-        // 환경변수로 받아올 것임 -> yml 파일에서 전달하게
+    public static void main(String[] args) {
         String webhookUrl = System.getenv("SLACK_WEBHOOK_URL");
+        String message = System.getenv("SLACK_WEBHOOK_MSG"); //환경변수 수정
 
-        // Java 11 -> fetch
+        if (webhookUrl == null || webhookUrl.isEmpty()) {
+            System.err.println("SLACK_WEBHOOK_URL 환경 변수가 설정되지 않았습니다.");
+            return;
+        }
+
+        if (message == null || message.isEmpty()) {
+            System.err.println("SLACK_WEBHOOK_MSG 환경 변수가 설정되지 않았습니다.");
+            return;
+        }
+
         HttpClient client = HttpClient.newHttpClient();
-        // 요청을 얹힐 거다
         HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(webhookUrl))
-            .header("Content-Type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"테스트 메시지\"}")) 
-            .build();
-        
+                .uri(URI.create(webhookUrl))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"" + message + "\"}")) // 메시지 구성 수정
+                .build();
+
         try {
-            HttpResponse<String> response = client.send(
-                request, HttpResponse.BodyHandlers.ofString()
-            );
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             System.out.println("요청 코드: " + response.statusCode());
             System.out.println("응답 결과: " + response.body());
         } catch (Exception e) {

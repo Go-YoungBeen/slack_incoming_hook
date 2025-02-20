b49bcf23b52bafa9ffd0324d7dc7853b2775c743
YoungBeen <120788477+Go-YoungBeen@users.noreply.github.com>
2025-02-20 17:59:01 +0900

    add

--- Bot.java
+++ Bot.java
@@ -9,29 +9,6 @@ public class Bot {
         // 웹훅을 만들 거임 -> URL 필요함
         // 환경변수로 받아올 것임 -> yml 파일에서 전달하게
         String webhookUrl = System.getenv("SLACK_WEBHOOK_URL");
-        String message = System.getenv("SLACK_WEBHOOK_MSG");
-
-        // LLM 파트
-        String llmUrl = System.getenv("LLM_URL");
-        String llmKey = System.getenv("LLM_KEY");
-
-        HttpClient llmClient = HttpClient.newHttpClient();
-        HttpRequest llmRequest = HttpRequest.newBuilder()
-            .uri(URI.create(llmUrl))
-            .header("Content-Type", "application/json")
-            .header("Authorization", "Bearer " + llmKey)
-            .POST(HttpRequest.BodyPublishers.ofString(
-                "{\"model\": \"meta-llama/Llama-3.3-70B-Instruct-Turbo\",\"messages\": [" + message + "],}"
-            )).build();
-        try {
-            HttpResponse<String> llmResponse = llmClient.send(
-                llmRequest, HttpResponse.BodyHandlers.ofString()
-            );
-            System.out.println("요청 코드: " + llmResponse.statusCode());
-            System.out.println("응답 결과: " + llmResponse.body());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }        
 
         // Java 11 -> fetch
         HttpClient client = HttpClient.newHttpClient();
@@ -39,9 +16,7 @@ public class Bot {
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(webhookUrl))
             .header("Content-Type", "application/json")
-            // .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"테스트 메시지\"}")) 
-            // .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\" + " + message + "\"}")) 
-            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\" + " + llmResponse.body() + "\"}")) 
+            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"테스트 메시지\"}")) 
             .build();
         
         try {

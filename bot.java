731a9ecfddf1747fa0860522f7c55648da68bbe9
YoungBeen <120788477+Go-YoungBeen@users.noreply.github.com>
2025-02-20 16:50:19 +0900

    main

--- bot.java
+++ bot.java
@@ -1,2 +1,19 @@
 import java.net.*;
-import java.net.http.*
+import java.net.http.*;
+import java.time.*;
+import java.util.*;
+
+public class Bot {
+    public class Bot {
+        public static void main(String[] args){
+            
+            String webhookUrl = System.getenv("SLACK_WEBHOOK_URL");
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+            .uri()
+
+
+        }
+    }
+}

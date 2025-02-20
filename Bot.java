d76e10ee22ea0735c3e94600d8f502e3e75a8c9a
YoungBeen <120788477+Go-YoungBeen@users.noreply.github.com>
2025-02-20 17:49:00 +0900

    ㅇㅈ

--- Bot.java
+++ Bot.java
@@ -4,16 +4,19 @@ import java.time.*;
 import java.util.*;
 
 public class Bot {
-    public static void main(String[] args) { 
+    public static void main(String[] args) { // 진입부분
+        // 이게 있어야 이 클래스를 실행했을 때 작동을 함
+        // 웹훅을 만들 거임 -> URL 필요함
+        // 환경변수로 받아올 것임 -> yml 파일에서 전달하게
         String webhookUrl = System.getenv("SLACK_WEBHOOK_URL");
-        String message = System.getenv("SLACK_WEBHOOK_URL");
 
+        // Java 11 -> fetch
         HttpClient client = HttpClient.newHttpClient();
+        // 요청을 얹힐 거다
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(webhookUrl))
             .header("Content-Type", "application/json")
-            // .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"테스트 메시지\"}")) 
-            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\" + " + message + "\"}")) 
+            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"테스트 메시지\"}")) 
             .build();
         
         try {
